d35534e1c69bafd2198e4cc4c34fd58f976929d6
Dangereye <craig_puxty@talktalk.net>
2020-11-26 19:43:06 +0000

    Added Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+
+import React, { Component } from 'react'
+
+class Navbar extends Component {
+    render() {
+        return (
+            <nav>
+                <div className="logo">
+                    <h1>Context App</h1>
+                    <ul>
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Contact</li>
+                    </ul>
+                </div>
+            </nav>
+        )
+    }
+}
+
+export default Navbar
